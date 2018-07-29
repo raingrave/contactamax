2cedb6dd3571818b8b60902714233e0262899644
Raingrave <raintech@outlook.com>
2018-07-29 03:12:20 -0300

    pré criação de migrations

---
+++ database/migrations/2018_07_28_043216_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateCategoriesTable extends Migration
+{
+    use SoftDeletes;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->text('description');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/2018_07_28_043317_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateProductsTable extends Migration
+{
+    use SoftDeletes;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id');
+            $table->string('sku', 65);
+            $table->string('name', 150);
+            $table->text('description');
+            $table->integer('quantity');
+            $table->decimal('price', 8, 2);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
